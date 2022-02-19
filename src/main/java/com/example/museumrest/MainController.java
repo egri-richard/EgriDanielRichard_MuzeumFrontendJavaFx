4c924c44a4960be9ec876daae98d1f52d743f8d3
egri-richard <egri.dani.r@gmail.com>
2022-02-19 21:54:15 +0100

    data classes + main controller

--- src/main/java/com/example/museumrest/MainController.java
+++ src/main/java/com/example/museumrest/MainController.java
@@ -1,14 +1,101 @@
 package com.example.museumrest;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
 
 public class MainController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @javafx.fxml.FXML
+    private TableView<Painting> paintingTable;
+    @javafx.fxml.FXML
+    private TableColumn<Painting, Boolean> paintingOnDisplayCol;
+    @javafx.fxml.FXML
+    private TableView<Statue> statueTable;
+    @javafx.fxml.FXML
+    private TableColumn<Painting, Integer> paintingDateCol;
+    @javafx.fxml.FXML
+    private TableColumn<Painting, String> paintingNameCol;
+    @javafx.fxml.FXML
+    private TableColumn<Statue, String> statuePersonCol;
+    @javafx.fxml.FXML
+    private Tab paintingPane;
+    @javafx.fxml.FXML
+    private Tab statuePane;
+    @javafx.fxml.FXML
+    private TableColumn<Statue, Integer> statueHeightCol;
+    @javafx.fxml.FXML
+    private TableColumn<Statue, Integer> statuePriceCol;
+    @javafx.fxml.FXML
+    private TabPane tabPane;
+    @javafx.fxml.FXML
+    private Button btnEdit;
+    @javafx.fxml.FXML
+    private Button btnDelete;
+    @javafx.fxml.FXML
+    private Button btnAdd;
+
+    public void initialize() {
+        paintingNameCol.setCellValueFactory(new PropertyValueFactory<>("title"));
+        paintingDateCol.setCellValueFactory(new PropertyValueFactory<>("year"));
+        paintingOnDisplayCol.setCellValueFactory(new PropertyValueFactory<>("on_display"));
+
+        statuePersonCol.setCellValueFactory(new PropertyValueFactory<>("person"));
+        statueHeightCol.setCellValueFactory(new PropertyValueFactory<>("height"));
+        statuePriceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+
+        try {
+            fillTable();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void addItem(ActionEvent actionEvent) {
+        int selectedTab = tabPane.getSelectionModel().getSelectedIndex();
+        if (selectedTab == 0) {
+            //Api.addPainting();
+        } else {
+            //Api.addStatue();
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void editItem(ActionEvent actionEvent) {
+        int selectedTab = tabPane.getSelectionModel().getSelectedIndex();
+        if (selectedTab == 0) {
+
+        } else {
+
+        }
+    }
+
+    @javafx.fxml.FXML
+    public void deleteItem(ActionEvent actionEvent) {
+        int selectedTab = tabPane.getSelectionModel().getSelectedIndex();
+        if (selectedTab == 0) {
+
+        } else {
+
+        }
+    }
+
+    private void fillTable() throws IOException, ParseException {
+        /*List<Painting> allP = Api.getPaintings();
+        paintingTable.getItems().clear();
+        for (Painting p: allP) {
+            paintingTable.getItems().add(p);
+        }
+
+        List<Statue> allS = Api.getStatues();
+        statueTable.getItems().clear();
+        for (Statue s: allS) {
+            statueTable.getItems().add(s);
+        }*/
     }
 }
--- src/main/java/com/example/museumrest/Painting.java
+++ src/main/java/com/example/museumrest/Painting.java
@@ -1,4 +1,52 @@
 package com.example.museumrest;
 
 public class Painting {
+    int id;
+    String title;
+    int year;
+    boolean on_display;
+
+    public Painting(int id, String title, int year, boolean on_display) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.on_display = on_display;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public boolean getOn_display() {
+        return on_display;
+    }
+
+    public void setOn_display(boolean on_display) {
+        this.on_display = on_display;
+    }
+
+    @Override
+    public String toString() {
+        return "id: " +id+ " title: " +title+ " year: " +year+ " On display: " +on_display+ "\n";
+    }
 }
