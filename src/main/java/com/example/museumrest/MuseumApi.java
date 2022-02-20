fc434a53f07d16a82070a7fdf465cb0090f86b44
egri-richard <egri.dani.r@gmail.com>
2022-02-20 14:18:23 +0100

    API fully implemented

--- src/main/java/com/example/museumrest/MuseumApi.java
+++ src/main/java/com/example/museumrest/MuseumApi.java
@@ -34,4 +34,80 @@ public class MuseumApi {
         Type type = new TypeToken<List<Statue>>(){}.getType();
         return jsonConvert.fromJson(json,type);
     }
+
+    public static Painting addPainting(Painting newP) throws IOException {
+        Gson jsonConvert = new Gson();
+        String paintingJson = jsonConvert.toJson(newP);
+        Response response = RequestHandler.post(PAINTING_URL, paintingJson);
+
+        String json = response.getContent();
+        if (response.getResponseCode() >= 400){
+            String message = jsonConvert.fromJson(json, ApiError.class).getMessage();
+            throw new IOException(message);
+        }
+        return jsonConvert.fromJson(json, Painting.class);
+    }
+
+    public static Statue addStatue(Statue newStatue) throws IOException {
+        Gson jsonConvert = new Gson();
+        String statueJson = jsonConvert.toJson(newStatue);
+        Response response = RequestHandler.post(STATUES_URL, statueJson);
+
+        String json = response.getContent();
+        if (response.getResponseCode() >= 400){
+            String message = jsonConvert.fromJson(json, ApiError.class).getMessage();
+            throw new IOException(message);
+        }
+        return jsonConvert.fromJson(json, Statue.class);
+    }
+
+    public static Painting editPainting(Painting toEdit) throws IOException {
+        Gson jsonConvert = new Gson();
+        String paintingJson = jsonConvert.toJson(toEdit);
+        Response response = RequestHandler.put(PAINTING_URL + "/" + toEdit.getId(), paintingJson);
+
+        String json = response.getContent();
+        if (response.getResponseCode() >= 400){
+            String message = jsonConvert.fromJson(json, ApiError.class).getMessage();
+            throw new IOException(message);
+        }
+        return jsonConvert.fromJson(json, Painting.class);
+    }
+
+    public static Statue editStatue(Statue toEdit) throws IOException {
+        Gson jsonConvert = new Gson();
+        String statueJson = jsonConvert.toJson(toEdit);
+        Response response = RequestHandler.put(STATUES_URL + "/" + toEdit.getId(), statueJson);
+
+        String json = response.getContent();
+        if (response.getResponseCode() >= 400){
+            String message = jsonConvert.fromJson(json, ApiError.class).getMessage();
+            throw new IOException(message);
+        }
+        return jsonConvert.fromJson(json, Statue.class);
+    }
+
+    public static boolean deletePainting(int id) throws IOException {
+        Response response = RequestHandler.delete(PAINTING_URL+ "/" + id);
+
+        Gson jsonConvert = new Gson();
+        String json = response.getContent();
+        if (response.getResponseCode() >= 400){
+            String message = jsonConvert.fromJson(json, ApiError.class).getMessage();
+            throw new IOException(message);
+        }
+        return response.getResponseCode() == 204;
+    }
+
+    public static boolean deleteStatue(int id) throws IOException {
+        Response response = RequestHandler.delete(STATUES_URL+ "/" + id);
+
+        Gson jsonConvert = new Gson();
+        String json = response.getContent();
+        if (response.getResponseCode() >= 400){
+            String message = jsonConvert.fromJson(json, ApiError.class).getMessage();
+            throw new IOException(message);
+        }
+        return response.getResponseCode() == 204;
+    }
 }
